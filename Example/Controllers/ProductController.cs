1b1207a4a3390f8a3e8380707b10f303c1c81fa0
Cesar Ceron <ceron.cmcr@gmail.com>
2024-09-07 12:32:05 -0600

    Initial commit

---
+++ Example/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Example.Models;
+using Example.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Example.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductRepository _repository = new ProductRepository();
+        // GET: Product
+        public ActionResult Index(string search)
+        {
+            var products = new List<Product>();
+            if (String.IsNullOrEmpty(search))
+            {
+                products = _repository.ObtenerProductos();
+            }
+            else {
+                products = _repository.SearchProduct(search);
+            }
+            
+            return View(products);
+        }
+    }
+}
---
+++ Example/Repository/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using Example.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Example.Repository
+{
+    public class ProductRepository
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        public List<Product> ObtenerProductos()
+        {
+            var products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();                
+                string query = $"SELECT id, name, price FROM Product";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    products.Add(new Product
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Name = reader["name"].ToString(),
+                        Price = Convert.ToDecimal(reader["price"])
+                    });
+                }
+
+                reader.Close();
+            }
+
+            return products;
+        }
+
+        public List<Product> SearchProduct(string search)
+        {
+            var products = new List<Product>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = $"SELECT id, name, price FROM Product where name like '%{search}%'";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    products.Add(new Product
+                    {
+                        Id = Convert.ToInt32(reader["id"]),
+                        Name = reader["name"].ToString(),
+                        Price = Convert.ToDecimal(reader["price"])
+                    });
+                }
+
+                reader.Close();
+            }
+
+            return products;
+        }
+    }
+}
